b11cafce7f31e94f2a4a2017dc12a433ee711ead
JonathanRedrado <jonathan.redrado98@gmail.com>
2023-04-18 22:38:35 -0300

    Version inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import groups from "./assets/groups.json"
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <>
+      <article className='card'>
+        <h3 className='card__id'>{groups.id}</h3>
+        <h4 className='card__group__name'>Nombre de grupo!</h4>
+        <h5 className='card__group__Leader'>{groups.leader}</h5>
+        <ul className='card__group__members'>
+          {groups.members.map( e => 
+            <li className='card__group__member'>{e}</li>
+            )}
+        </ul>
+      </article>
+      <footer className='footer'>Projecto inicial CodoACodo React</footer>
+    </>
   )
 }
 
